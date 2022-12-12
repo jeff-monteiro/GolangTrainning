bb8c277196a001dbc1fb795173a9433fc59b9416
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-12-12 16:33:11 -0300

    kotlin fundamentals

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -5,8 +5,18 @@ fun main(){
     var balanceAccount: Double = 0.00
     balanceAccount = 100.00
     balanceAccount += 20.00
+    balanceAccount -= 1000
 
     println("Holder: $holder")
     println("Account number: $accountNumber")
     println("Balance: $balanceAccount")
+
+    // The WHEN expression can be used to build this example.
+    if(balanceAccount > 0.00){
+        println("Account has positive balance")
+    } else if(balanceAccount == 0.00){
+        println("Neutral balance")
+    } else {
+        println("Negative balance")
+    }
 }
