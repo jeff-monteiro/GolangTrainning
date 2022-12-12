c0960f9ff1662a1977fb8d45ce3fd6e9b7781845
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2022-12-12 16:06:17 -0300

    kt fundamentals

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 fun main(){
-    println("Bem vindo ao Bytebank")
-    val titular = "Jefferson Monteiro"
-    print("titular $titular")
+    println("Welcome to the Bytebank")
+    val holder: String = "Jefferson Monteiro"
+    val accountNumber: Int = 1045
+    var balanceAccount: Double = 0.00
+    balanceAccount = 100.00
+    balanceAccount += 20.00
+
+    println("Holder $holder")
+    println("Account number : $accountNumber")
+    println("Balance: $balanceAccount")
 }
