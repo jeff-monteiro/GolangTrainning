00fd7236708c6aecdb131cd874292c5cc92a6481
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2023-04-22 20:33:23 -0300

    switch_command

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -16,13 +16,24 @@ func main() {
 	fmt.Scan(&comando) //That's a reference variable
 	fmt.Println("O comando escolhido foi", comando)
 
-	if comando == 1 {
+	// if comando == 1 {
+	// 	fmt.Println("Iniciando Monitoramento...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo Logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do Programa")
+	// } else {
+	// 	fmt.Println("Não conheço esse comando")
+	// }
+
+	switch comando {
+	case 1:
 		fmt.Println("Iniciando Monitoramento...")
-	} else if comando == 2 {
+	case 2:
 		fmt.Println("Exibindo Logs...")
-	} else if comando == 0 {
+	case 0:
 		fmt.Println("Saindo do Programa")
-	} else {
+	default:
 		fmt.Println("Não conheço esse comando")
 	}
 }
