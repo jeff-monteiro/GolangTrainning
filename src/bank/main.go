8b595fe01be003fb41353356de2091de83c59c91
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2023-04-27 15:57:31 -0300

    using structs

--- src/bank/main.go
+++ src/bank/main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+type ContaCorrente struct {
+	titular       string
+	numeroConta   int
+	numeroAgencia int
+	saldoConta    float64
+}
+
 func main() {
-	fmt.Println("Starting...")
+	contaJefferson := ContaCorrente{"Jefferson", 589, 256934, 3000.05}
+	contaJoelma := ContaCorrente{"Joelma", 123, 568784, 7000.01}
+	fmt.Println(contaJefferson)
+	fmt.Println(contaJoelma)
+
 }
