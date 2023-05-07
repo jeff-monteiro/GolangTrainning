baeabaad4b4b6460babd99cd8046edb39455f6af
Jefferson <jeffmonteiro61@icloud.com>
2023-05-07 13:54:12 -0300

    package_contas

---
+++ src/bank/contas/contaCorrente.go
@@ -0,0 +1,41 @@
+package contas
+
+type ContaCorrente struct {
+	Titular       string
+	NumeroConta   int
+	NumeroAgencia int
+	SaldoConta    float64
+}
+
+// Calculate withdraw from TPM
+func (c *ContaCorrente) Sacar(valorSaque float64) string {
+	podeSacar := valorSaque > 0 && valorSaque <= c.SaldoConta
+	if podeSacar {
+		c.SaldoConta -= valorSaque
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saque não realizado"
+	}
+}
+
+// Calculate deposit from TPM || função com multiplos retornos
+func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.SaldoConta += valorDoDeposito
+		return "Deposito feito com sucesso!", c.SaldoConta
+	} else {
+		return "O valor do depósito menor que zero!", c.SaldoConta
+	}
+
+}
+
+// Make transfer between accounts
+func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia <= c.SaldoConta && valorDaTransferencia > 0 {
+		c.SaldoConta -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
--- src/bank/main.go
+++ src/bank/main.go
@@ -1,50 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type ContaCorrente struct {
-	titular       string
-	numeroConta   int
-	numeroAgencia int
-	saldoConta    float64
-}
-
-// Calculate withdraw from TPM
-func (c *ContaCorrente) Sacar(valorSaque float64) string {
-	podeSacar := valorSaque > 0 && valorSaque <= c.saldoConta
-	if podeSacar {
-		c.saldoConta -= valorSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saque não realizado"
-	}
-}
-
-// Calculate deposit from TPM || função com multiplos retornos
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldoConta += valorDoDeposito
-		return "Deposito feito com sucesso!", c.saldoConta
-	} else {
-		return "O valor do depósito menor que zero!", c.saldoConta
-	}
-
-}
-
-// Make transfer between accounts
-func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTransferencia <= c.saldoConta && valorDaTransferencia > 0 {
-		c.saldoConta -= valorDaTransferencia
-		contaDestino.Depositar(valorDaTransferencia)
-		return true
-	} else {
-		return false
-	}
-}
+	"bank/contas"
+)
 
 func main() {
-	contaJefferson := ContaCorrente{titular: "Jefferson", saldoConta: 300}
-	contaMaria := ContaCorrente{titular: "Maria", saldoConta: 200}
+	contaJefferson := contas.ContaCorrente{Titular: "Jefferson", SaldoConta: 300}
+	contaMaria := contas.ContaCorrente{Titular: "Maria", SaldoConta: 200}
 
 	status := contaJefferson.Transferir(100, &contaMaria)
 	fmt.Println(status)
