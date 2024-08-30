d2cf7602e062b89b36d5a5a57cc4b9fb1a8870a5
Jefferson Monteiro <81328013+jeff-monteiro@users.noreply.github.com>
2024-08-29 23:52:33 -0300

    Update data-types

--- src/GoFundamentals/3 - Tipos de Dados/tipos-de-dados.go
+++ src/GoFundamentals/3 - Tipos de Dados/tipos-de-dados.go
@@ -3,6 +3,41 @@ package main
 import "fmt"
 
 func main() {
+
+	// INT INICIO
 	var numero int64 = 1000000000000000000
 	fmt.Println(numero)
+
+	var numero2 uint32 = 1000
+	fmt.Println(numero2)
+
+	//alias
+	//RUNE é um alias para int32
+	//BYTE é um alias para uint8
+
+	//INT FIM
+
+	//FLOAT INICIO
+
+	var number3 float32 = 123.45
+	fmt.Println(number3)
+
+	var number4 float64 = 12365.89
+	fmt.Println(number4)
+
+	//FLOAT FIM
+
+	//STRING INICIO
+
+	var nomeVariavel1 string = "Nome Da Variavel1"
+	fmt.Println(nomeVariavel1)
+
+	var nomeVariavel2 = "Nome Da Variavel2"
+	fmt.Println(nomeVariavel2)
+
+	char := 'B'
+	fmt.Println(char)
+
+	//STRING FIM
+
 }
