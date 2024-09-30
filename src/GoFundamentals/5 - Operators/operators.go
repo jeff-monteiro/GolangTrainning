b1195fda51105929e6f57bef7f505e59ce220eb6
Jefferson Monteiro <jeffmonteiro61@outlook.com>
2024-09-30 14:42:42 -0300

    operatorsGo

---
+++ src/GoFundamentals/5 - Operators/operators.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Arithmetics Operators
+	soma := 10 + 2
+	subtracao := 10 - 2
+	multi := 10 * 2
+	divisao := 10 / 2
+	restoDaDivisao := 10 % 2
+
+	fmt.Println(soma, subtracao, multi, divisao, restoDaDivisao)
+}
