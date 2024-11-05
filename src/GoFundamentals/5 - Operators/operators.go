8f56e651fc9730b293f730125498179dd8c93065
Jefferson Monteiro <81328013+jeff-monteiro@users.noreply.github.com>
2024-11-04 23:29:54 -0300

    Update Operators
    operators_used_on_golang

--- src/GoFundamentals/5 - Operators/operators.go
+++ src/GoFundamentals/5 - Operators/operators.go
@@ -11,4 +11,47 @@ func main() {
 	restoDaDivisao := 10 % 2
 
 	fmt.Println(soma, subtracao, multi, divisao, restoDaDivisao)
+	atribuitionOperators()
+	relationalOperators()
+	logicalOperators()
+	unaryOperators()
+}
+
+// Atribuition Operators
+func atribuitionOperators() {
+	var variavel string = "My name"
+	variavel2 := "This is Us"
+
+	fmt.Println(variavel, variavel2)
+}
+
+// Relational Operators
+func relationalOperators() {
+	fmt.Println(1 > 2)
+	fmt.Println(1 >= 2)
+	fmt.Println(1 == 2)
+	fmt.Println(1 < 2)
+	fmt.Println(1 <= 2)
+	fmt.Println(1 != 2)
+}
+
+// Logical Operators
+func logicalOperators() {
+	fmt.Println(true && true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
+
+// Unary Operators
+func unaryOperators() {
+	temp := 10
+	temp++     // temp = temp + 1
+	temp += 15 // temp = temp + 15
+	temp--     // temp = temp - 1
+
+	temp *= 3 // temp = temp * 3
+	temp /= 2 // temp = temp / 2
+	temp %= 2 // temp = temp % 2
+	fmt.Println(temp)
 }
