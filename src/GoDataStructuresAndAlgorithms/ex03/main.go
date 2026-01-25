d19adffc4856cbd23c6766da3946751b6611cafb
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2026-01-25 16:01:46 -0300

    working-with-data-structures

--- src/GoDataStructuresAndAlgorithms/ex03/main.go
+++ src/GoDataStructuresAndAlgorithms/ex03/main.go
@@ -1,13 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 	var scores = []int{90, 70, 50, 60, 80, 85}
 	var length = len(scores)
 	var tempArray = make([]int, length+1)
 
-	insert(scores, tempArray, 75, 2)
+	scores = slices.Insert(tempArray, 2, 75)
 
 	scores = tempArray
 
